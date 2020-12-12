20bc4828ac8ce0fa74bee4a53addc07e0fc244a2
FabienRoussel <fabien.roussel921@gmail.com>
2020-12-12 09:02:43 +0100

    Day 12 done

--- src/main/scala/aventofcode/Day12.scala
+++ src/main/scala/aventofcode/Day12.scala
@@ -21,7 +21,6 @@ object Day12 {
     val update: Int = char match {
       case 'L' => -int / 90
       case 'R' => int / 90
-      case 'F' => 0
       case _ => 0
     }
     INT_TO_DIRECTIONS( (update + DIRECTIONS_TO_INT(prevDir)  +4) % 4)
@@ -59,8 +58,63 @@ object Day12 {
     computeManhattanDistance(0, 0, posX, posY)
   }
 
+  // PART II
+  def computeNewDirectionForLRWaypoint(prevWaypointX: Int, prevWaypointY: Int, char: Char, int: Int): (Int, Int) = {
+    val (newWaypointX, newWaypointY): (Int, Int) = char match {
+      case 'L' =>
+        int match {
+          case 90 => (prevWaypointY, -prevWaypointX)
+          case 180 => (-prevWaypointX, -prevWaypointY)
+          case 270 => (-prevWaypointY, prevWaypointX)
+        }
+      case 'R' =>
+        int match {
+          case 90 => (-prevWaypointY, prevWaypointX)
+          case 180 => (-prevWaypointX, -prevWaypointY)
+          case 270 => (prevWaypointY, -prevWaypointX)
+        }
+      case 'N' | 'S' | 'E' | 'W' => (prevWaypointX + DIRECTIONS_TO_AXEX(char)*int, prevWaypointY + DIRECTIONS_TO_AXEY(char)*int)
+      case _ => (prevWaypointX, prevWaypointY)
+    }
+    (newWaypointX, newWaypointY)
+  }
+
+  def computeNewPositionWaypoint(posX: Int, posY: Int, waypointX: Int, waypointY: Int, instructChar: Char, instructInt: Int): (Int, Int) = {
+    val (newPosX, newPosY): (Int, Int) = instructChar match {
+      case 'F' => (posX + waypointX * instructInt, posY +  waypointY * instructInt)
+      case _ => (posX, posY)
+    }
+    (newPosX, newPosY)
+  }
+
+  def computePositionWaypoint(posX: Int, posY: Int, waypointX: Int, waypointY: Int, instruction: String): (Int, Int, Int, Int) = {
+    val (instructChar, instructInt) = extractInstruction(instruction)
+    val (newWaypointX, newWaypointY) = computeNewDirectionForLRWaypoint(waypointX, waypointY, instructChar, instructInt)
+    val (newPosX, newPosY) = computeNewPositionWaypoint(posX, posY, newWaypointX, newWaypointY, instructChar, instructInt)
+
+    (newPosX, newPosY, newWaypointX, newWaypointY)
+  }
+
+  def challenge2(): Int = {
+    var posX = 0
+    var posY = 0
+    var waypointX = 10
+    var waypointY = -1
+    val file      = ReadFile.readFile("jour12.txt")
+
+    file.foreach(el => {
+      val (newPosX, newPosY, newWaypointX, newWaypointY) = computePositionWaypoint(posX, posY, waypointX, waypointY, el)
+      posX = newPosX
+      posY = newPosY
+      waypointX = newWaypointX
+      waypointY = newWaypointY
+    })
+    computeManhattanDistance(0, 0, posX, posY)
+  }
+
   def main(args: Array[String]): Unit = {
     println("Day 10 !")
     println("Challenge 1 : " + challenge1())
+    println("Challenge 1 : " + challenge2())
   }
 }
