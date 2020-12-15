b32a3f1280c3c8897540924f664a76af50b8ac39
FabienRoussel <fabien.roussel921@gmail.com>
2020-12-15 09:24:28 +0100

    Day 15 + optimisation

--- src/main/scala/aventofcode/Day15.scala
+++ src/main/scala/aventofcode/Day15.scala
@@ -5,21 +5,20 @@ object Day15 {
   // For int given / last time this int was given
   var memory: Map[Int, Int] = Map[Int, Int]()
 
-  def challenge1(numbersList: List[Int], xThNumberSpoken: Int): Int = {
-    if(xThNumberSpoken == 0) return numbersList.last
-    val lastNumber = numbersList.last
+  def challenge1(lastNumber: Int, xThNumberSpoken: Int, turn: Int): Int = {
+    if(xThNumberSpoken == 0) return lastNumber
     val newNumber = {
       if (memory.contains(lastNumber)) {
-        val difference = numbersList.size - memory(lastNumber)
-        memory += (lastNumber -> numbersList.size)
+        val difference = turn - memory(lastNumber)
+        memory += (lastNumber -> turn)
         difference
       }
       else {
-        memory += (lastNumber -> numbersList.size)
+        memory += (lastNumber -> turn)
         0
       }
     }
-    challenge1(numbersList :+ newNumber, xThNumberSpoken - 1)
+    challenge1(newNumber, xThNumberSpoken - 1, turn + 1)
   }
 
   def main(args: Array[String]): Unit = {
@@ -27,10 +26,10 @@ object Day15 {
     println("Day 15 !")
 
     for (it <- 0 until list.size - 1) memory += (list(it) -> (it + 1))
-    println("Challenge 1 : " + challenge1(list, 2020 - list.size))
+    println("Challenge 1 : " + challenge1(list.last , 2020 - list.size, list.size))
 
     memory = Map[Int, Int]()
     for (it <- 0 until list.size - 1) memory += (list(it) -> (it + 1))
-    println("Challenge 2 : " + challenge1(list, 30000000 - 3))
+    println("Challenge 2 : " + challenge1(list.last, 30000000 - list.size, list.size))
   }
 }
